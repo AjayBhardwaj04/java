b43770095be084d96d17d7e396adf0ae1e9ded70
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-09-24 04:53:17 +0530

    java loop file push

--- com.java
+++ com.java
@@ -1,5 +1,9 @@
 public class com {
     public static void main(String[] args) {
-        System.out.println("asd");
+        for (int num = 1; num <= 10; num += 1){
+            System.out.println(num);
+        }
+
+
     }
 }
