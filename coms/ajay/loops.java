1cc6194394de1eb6f9c3b49deb6a4e46d19b3fcf
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-09-23 20:07:39 +0530

    Java DSA code commit

---
+++ coms/ajay/loops.java
@@ -0,0 +1,19 @@
+package coms.ajay;
+
+public class loops {
+    public static void main(String[] args) {
+        System.out.println("ajay");
+        /*
+         Syntax of For loop
+for(initialisation,condition,increment/decrement){
+// body
+}
+   */
+
+for (int num=1;num <=10; num +=1 ){
+    System.out.println(num);
+}
+
+    }
+}
+
---
+++ condition.java
@@ -0,0 +1,5 @@
+public class condition {
+    public static void main(String[] args) {
+
+    }
+}
