fef384af475c4d0e540a588aa2d06622e6ef6d99
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-09-24 05:58:09 +0530

    java dsa loop file commit

--- com.java
+++ com.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class com {
     public static void main(String[] args) {
-        for (int num = 1; num <= 10; num += 1){
-            System.out.println(num);
-        }
 
+//        for loops
+//        for (int num = 1; num <= 10; num += 1){
+//            System.out.println(num);
+//        }
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 1; i <=n; i++) {
+            System.out.println( "Hello Ajay -" + i);
+        }
 
     }
 }
--- coms/ajay/loops.java
+++ coms/ajay/loops.java
@@ -1,19 +1,49 @@
 package coms.ajay;
 
+import java.util.Scanner;
+
 public class loops {
     public static void main(String[] args) {
         System.out.println("ajay");
-        /*
-         Syntax of For loop
+ /*
+ Syntax of For loop
 for(initialisation,condition,increment/decrement){
 // body
 }
-   */
+ */
 
-for (int num=1;num <=10; num +=1 ){
-    System.out.println(num);
+//for (int num=1;num <=10; num +=1 ){
+//    System.out.println(num);
+//}
+//
+//
+//Scanner input = new Scanner(System.in);
+//int n = input.nextInt();
+//for (int i = 1; i <=n; i++) {
+//System.out.println( i);
+//         }
+
+//While Loops
+//        Syntax:
+//        while(Condition){
+//        Body };
+//          int num = 0;
+//while (num <= 20  ){
+//    System.out.println(num);
+//    num += 2;
+//}
+
+
+//        Do while loops
+//        Syntax:
+//        do{
+//        Body } while(condition)
+
+int n = 0 ;
+do {
+    System.out.println(n);
+    n++;
+}  while (n <= 7);
+
+       }
 }
-
-    }
-}
-
