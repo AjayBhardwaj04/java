246b5cdb377322f8fe640528e0c6f4634c15b196
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-09-28 07:12:52 +0530

    Counts  Reverse num. using loops

--- coms/ajay/countReNum.java
+++ coms/ajay/countReNum.java
@@ -1,2 +1,14 @@
-package coms.ajay;public class countReNum {
+package coms.ajay;
+
+public class countReNum {
+    public static void main(String[] args) {
+        int n = 93421;
+        int count = 0;
+        while (n > 0){
+         int revNub = n % 10;   
+         n = n / 10;
+
+            System.out.print(String.valueOf(revNub));
+        }
+    }
 }
