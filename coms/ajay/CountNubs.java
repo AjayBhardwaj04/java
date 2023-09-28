1368e242192dcc16f6f8c5e9d0a83a7d5361fd70
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-09-28 06:55:32 +0530

    counts num.

--- coms/ajay/CountNubs.java
+++ coms/ajay/CountNubs.java
@@ -1,2 +1,17 @@
-package coms.ajay;public class CountNubs {
+package coms.ajay;
+
+public class CountNubs {
+    public static void main(String[] args) {
+        int n = 1353353;
+        int count = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            if (rem == 3) {
+                count++;
+            }
+n = n / 10;
+        }
+        System.out.printf(String.valueOf(count));
+    }
+
 }
