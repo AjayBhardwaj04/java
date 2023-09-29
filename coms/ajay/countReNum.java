551ee13d1e558033d651999830b9c15ab3440c2c
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-09-29 06:26:27 +0530

    last commit

--- coms/ajay/countReNum.java
+++ coms/ajay/countReNum.java
@@ -6,9 +6,10 @@ public class countReNum {
         int count = 0;
         while (n > 0){
          int revNub = n % 10;   
-         n = n / 10;
+         n = n / 10 ;
 
-            System.out.print(String.valueOf(revNub));
+//            System.out.print(String.valueOf(revNub));
+            System.out.print(revNub);
         }
-    }
+      }
 }
