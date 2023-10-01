8dfec44ba4a874ef7ee8a8ebb7f3e8c1925fcad6
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-01 07:11:52 +0530

    Swith case lecture

--- coms/ajay/Switch.java
+++ coms/ajay/Switch.java
@@ -1,2 +1,41 @@
-package coms.ajay;public class Switch {
+package coms.ajay;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String fruits = in.next();
+
+//        switch (fruits){
+//
+//            case "mango":
+//                System.out.println("King fo fruits");
+//                break;
+//            case "banana":
+//                System.out.println(" Banana is  soft fruits");
+//                break;
+//            case "Grapes":
+//                System.out.println("Grapes is group fruits");
+//                break;
+//            case "apple":
+//                System.out.println("Apple is red  fruits");
+//                break;
+//            default:
+//                System.out.println("qadsafdaef sgsdf");
+//
+//        }
+
+        switch (fruits) {
+            case "mango" -> System.out.println("King fo fruits");
+            case "banana" -> System.out.println(" Banana is  soft fruits");
+            case "Grapes" -> System.out.println("Grapes is group fruits");
+            case "apple" -> System.out.println("Apple is red  fruits");
+            default -> System.out.println("qadsafdaef sgsdf");
+        }
+        System.out.println(fruits);
+
+
+        System.out.println(fruits);
+    }
 }
