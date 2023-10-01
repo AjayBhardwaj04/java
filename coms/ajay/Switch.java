cb58aa36d94c9290d09d82e45d7b7113c3cb6e6e
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-01 08:18:46 +0530

    Modify this code

--- coms/ajay/Switch.java
+++ coms/ajay/Switch.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Switch {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String fruits = in.next();
+//        String fruits = in.next();
+//        String day = in.next();
+        int day = in.nextInt();
 
 //        switch (fruits){
 //
@@ -26,16 +28,20 @@ public class Switch {
 //
 //        }
 
-        switch (fruits) {
-            case "mango" -> System.out.println("King fo fruits");
-            case "banana" -> System.out.println(" Banana is  soft fruits");
-            case "Grapes" -> System.out.println("Grapes is group fruits");
-            case "apple" -> System.out.println("Apple is red  fruits");
-            default -> System.out.println("qadsafdaef sgsdf");
+//        switch (fruits) {
+//            case "mango" -> System.out.println("King fo fruits");
+//            case "banana" -> System.out.println(" Banana is  soft fruits");
+//            case "Grapes" -> System.out.println("Grapes is group fruits");
+//            case "apple" -> System.out.println("Apple is red  fruits");
+//            default -> System.out.println("qadsafdaef sgsdf");
+//        }
+//        System.out.println(fruits);
+
+        switch (day) {
+            case 1, 2, 3, 4 -> System.out.println("Weekday");
+            case 5 -> System.out.println("Holiday");
         }
-        System.out.println(fruits);
 
 
-        System.out.println(fruits);
     }
 }
---
+++ coms/ajay/nestedSwitch.java
@@ -0,0 +1,2 @@
+package coms.ajay;public class nestedSwitch {
+}
