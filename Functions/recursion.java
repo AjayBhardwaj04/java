49fe27fed6c883eaad944a53d850f1609bf23f6a
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-02 22:02:36 +0530

    Modify code

--- Functions/recursion.java
+++ Functions/recursion.java
@@ -2,17 +2,30 @@ package Functions;
 
 import java.util.Scanner;
 
+import static java.lang.System.*;
+
 public class recursion {
     public static void main(String[] args) {
-        sum();
+       int ans = num2();
+        out.println(ans);
+    }
+
+    static int num2(){
+        Scanner in = new Scanner(System.in);
+        out.print("Inter Number 1 : ");
+        int num1 = in.nextInt();
+        out.print("Inter Number 2 : ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+       return sum ;
     }
     static  void sum(){
         Scanner in = new Scanner(System.in);
-        System.out.print("Inter Number 1 : ");
+        out.print("Inter Number 1 : ");
         int num1 = in.nextInt();
-        System.out.print("Inter Number 2 : ");
+        out.print("Inter Number 2 : ");
         int num2 = in.nextInt();
         int sum = num1 + num2;
-        System.out.println("The Sum is = " + sum);
+        out.println("The Sum is = " + sum);
     }
 }
