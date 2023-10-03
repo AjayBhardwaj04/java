82d6f6abd90bf010dd6e48ac9c27d8704798f439
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-03 18:55:04 +0530

    Update code

--- Functions/StringExample.java
+++ Functions/StringExample.java
@@ -9,7 +9,7 @@ public class StringExample {
         String a = in.next();
         System.out.println("Inter your Last name:  ");
         String b = in.next();
-        String mass = greet(a, b);
+        String mass = greet("Hello" + a ,b);
         System.out.println(mass);
     }
 static String greet(String  a, String b) {
