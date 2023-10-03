6db9ccb2549f05c5ad2aeb57fdd36a0bd4dd5908
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-03 18:57:01 +0530

    Swap Number code

--- Functions/swap.java
+++ Functions/swap.java
@@ -1,2 +1,13 @@
-package Functions;public class swap {
+package Functions;
+
+public class swap {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 40;
+//        swap number code
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a +"  " + b);
+    }
 }
