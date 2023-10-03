457f9c94c06422a65834eb1abd6524b89d44ef29
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-03 07:37:04 +0530

    Java Function String Exam.

---
+++ Functions/StringExample.java
@@ -0,0 +1,22 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class StringExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Inter your First name:  ");
+        String a = in.next();
+        System.out.println("Inter your Last name:  ");
+        String b = in.next();
+        String mass = greet(a, b);
+        System.out.println(mass);
+    }
+static String greet(String  a, String b) {
+
+String gateeing = a + b ;
+return gateeing;
+}
+
+}
+
