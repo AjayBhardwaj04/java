31d7deb144acaeef9dfeb9ecaaa2051825e56fa8
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-08 09:08:17 +0530

    code

--- Functions/PassingExample.java
+++ Functions/PassingExample.java
@@ -2,6 +2,14 @@ package Functions;
 
 public class PassingExample {
     public static void main(String[] args) {
+String name ="Ajay Raj";
+        System.out.println(name);
+greet(name);
 
+    }
+
+ static void greet(String naam) {
+
+         System.out.println(naam);
     }
 }
