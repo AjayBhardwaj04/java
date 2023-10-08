ba138802365e7329abb269636a30f061e81cddbf
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-08 09:34:41 +0530

    code

---
+++ Functions/changeValue.java
@@ -0,0 +1,16 @@
+package Functions;
+
+import java.util.Arrays;
+
+public class changeValue {
+    public static void main(String[] args) {
+//        create an array
+        int[] arr = {12,1,2,34,21,231};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void change(int[] nums){
+        nums[0] = 79;
+    }
+}
