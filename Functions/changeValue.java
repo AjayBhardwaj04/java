e9471095a90eeda55a541ff037f2185cc9907d1f
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-25 22:02:52 +0530

    modify

--- Functions/changeValue.java
+++ Functions/changeValue.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class changeValue {
     public static void main(String[] args) {
 //        create an array
-        int[] arr = {12,1,2,34,21,231};
+        int[] arr = {12,1,2,34,12,21,231};
         change(arr);
         System.out.println(Arrays.toString(arr));
     }
